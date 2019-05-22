9a933bd60ee37c1690affa36ab2c2953cbeaba27
Sameer Totey <sameertotey@hotmail.com>
2019-05-22 15:18:40 -0600

    Changed the gruntfile to the latest snippet

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,10 @@
 module.exports = (grunt) => {
-    grunt.registerTask('default', 'Hello World', () => grunt.log.writeln("##teamcity[buildProblemdescription='There is a problem with Hello World' identity='12345']"))
-}
+    grunt.registerTask('default', 'Example TeamCity Progress Messaging', function() {
+        const done = this.async();
+        grunt.log.writeln("##teamcity[progressMessage 'An example progress message. Waiting 5 seconds']");
+        setTimeout(() => {
+            grunt.log.writeln("##teamcity[progressMessage '5 seconds over, all done']");
+            done();
+        }, 5000);
+    });
+};
